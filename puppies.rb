ca7da9cd4371e85ca1a0f20944a9db5b3baea9fb
ehailu675 <elisabethahailu@gmail.com>
2018-07-25 16:03:24 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -4,7 +4,7 @@ def no_puppies(number)
   if number > 0
     puts "No more puppies!"
   else 
-      puts "Well done, Rachel!"
+      puts "Good job, Rachel!"
   end
 end
 # 2. Rachel's animal accumulation has been going well and we're going to cut her some slack. Write a new method called less_puppies that takes in a number of puppies as an argument and tells Rachel good job when she gets less than 3 puppies, to slow down if she's found exactly 3 puppies, and to get back on her animal restriction if she gets more than 3 puppies.
@@ -13,7 +13,7 @@ def less_puppies (number)
   if number < 3
     puts "Good job, Rachel!"
   elsif number == 3
-    puts "Slow down, Rachel!"
+    puts "Get back to your animal goals"
   else number > 3
     puts "Go back to animal restriction!"
   end
